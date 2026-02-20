9724e82bf981a17d12511bf2c915c907f7625af8
Malte Neemann <malteneemann129@gmail.com>
2026-02-20 13:59:19 +0100

    imporved UX and UI. Added Error handling and removed redundand code by creating extra function for loading data

--- filter_users.py
+++ filter_users.py
@@ -1,48 +1,83 @@
 import json
 
 
+def load_users():
+    """Lädt die User-Daten und fängt Datei-Fehler ab."""
+    try:
+        with open("users.json", "r") as file:
+            return json.load(file)
+
+    except FileNotFoundError:
+        print("Error: 'users.json' not found")
+        return []
+
+    except json.JSONDecodeError:
+        print("Error: 'users.json' not written in the right way")
+        return []
+
+
 def filter_users_by_name(name):
-    with open("users.json", "r") as file:
-        users = json.load(file)
+    users = load_users()
 
     filtered_users = [user for user in users if user["name"].lower() == name.lower()]
 
-    for user in filtered_users:
-        print(user)
+    if not filtered_users:
+        print("No Users found with that name\n")
+    else:
+        for user in filtered_users:
+            print(user)
 
 
 def filter_users_by_age(age):
-    with open("users.json", "r") as file:
-        users = json.load(file)
+    users = load_users()
 
     filtered_users = [user for user in users if user["age"] == age]
 
-    for user in filtered_users:
-        print(user)
+    if not filtered_users:
+        print("No Users found with that age\n")
+    else:
+        for user in filtered_users:
+            print(user)
 
 
 def filter_users_by_email(email):
-    with open("users.json", "r") as file:
-        users = json.load(file)
+    users = load_users()
 
     filtered_users = [user for user in users if user["email"].lower() == email.lower()]
 
-    for user in filtered_users:
-        print(user)
+    if not filtered_users:
+        print("No Users found with that email\n")
+    else:
+        for user in filtered_users:
+            print(user)
 
 
 if __name__ == "__main__":
-    filter_option = input("What would you like to filter by? (Currently, only 'name', 'age' and 'email' is supported): ").strip().lower()
+    while True:
+        filter_option = input("What would you like to filter by?"
+                              "(Currently, only 'name', 'age' and 'email' is supported."
+                              " Write 'exit' to leave): ").strip().lower()
 
-    if filter_option == "name":
-        name_to_search = input("Enter a name to filter users: ").strip()
-        filter_users_by_name(name_to_search)
-    elif filter_option == "age":
-        age_to_search = int(input("Enter a age to filter users: ").strip())
-        filter_users_by_age(age_to_search)
-    elif filter_option == "email":
-        email_to_search = input("Enter a email to filter users: ").strip()
-        filter_users_by_email(email_to_search)
-    else:
-        print("Filtering by that option is not yet supported.")
+        if filter_option == "name":
+            name_to_search = input("Enter a name to filter users: ").strip()
+            filter_users_by_name(name_to_search)
+
+        elif filter_option == "age":
+            try:
+                age_to_search = int(input("Enter a age to filter users: ").strip())
+                filter_users_by_age(age_to_search)
+            except ValueError:
+                print("We need a number\n")
+                continue
+
+        elif filter_option == "email":
+            email_to_search = input("Enter a email to filter users: ").strip()
+            filter_users_by_email(email_to_search)
+
+        elif filter_option == "exit":
+            print("goodbye")
+            break
+
+        else:
+            print("Filtering by that option is not yet supported.\n")
 
